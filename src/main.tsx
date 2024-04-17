71cac9f15c270aa3a0da5a55d5c7fcb158d0e59d
David <david.sehi2016@gmail.com>
2024-04-17 11:12:51 +0000

    📈 add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { ConfigProvider } from 'antd';
 import { Toaster } from 'react-hot-toast';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from '@/contexts/auth';
-import { ConfigProvider } from 'antd';
 import { App } from '@/app';
 import '@/global.css';
 
@@ -17,30 +18,33 @@ const queryClient = new QueryClient({
 const PRIMARY_COLOR = 'hsl(214.3 31.8% 91.4%)';
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <ConfigProvider
-        theme={{
-          components: {
-            DatePicker: {
-              activeBorderColor: PRIMARY_COLOR,
-              activeShadow: `0 0 0 0px ${PRIMARY_COLOR}`,
-              hoverBorderColor: PRIMARY_COLOR,
-              // TODO: ...
+  <>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ConfigProvider
+          theme={{
+            components: {
+              DatePicker: {
+                activeBorderColor: PRIMARY_COLOR,
+                activeShadow: `0 0 0 0px ${PRIMARY_COLOR}`,
+                hoverBorderColor: PRIMARY_COLOR,
+                // TODO: ...
+              },
+              Button: {
+                // TODO: Add custom button styles
+              },
             },
-            Button: {
-              // TODO: Add custom button styles
-            },
-          },
-        }}
-      >
-        <App />
-        <Toaster
-          toastOptions={{
-            duration: 4000,
           }}
-        />
-      </ConfigProvider>
-    </AuthProvider>
-  </QueryClientProvider>,
+        >
+          <App />
+          <Toaster
+            toastOptions={{
+              duration: 4000,
+            }}
+          />
+        </ConfigProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+    <Analytics />
+  </>,
 );
